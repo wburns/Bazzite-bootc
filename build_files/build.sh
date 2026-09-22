a6f242014a70b9227e8c6440dcbf5e69915adeb3
William Burns <mudokonman@gmail.com>
2026-09-21 23:35:03 -0700

    Need to enable plugins-core as well

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,9 @@ cp -avf "/ctx/system_files"/. /
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-dnf copr enable atim/starship
+# Enable copr repos
+dnf install -y dnf-plugins-core && \
+    dnf copr enable -y atim/starship
 
 # this installs a package from fedora repos
 dnf5 install -y tmux \
