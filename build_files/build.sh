1a02c4a3c852fe86872b752745fd4b76ecf0fc15
William Burns <mudokonman@gmail.com>
2026-09-21 23:06:58 -0700

    add kitty and starship

--- build_files/build.sh
+++ build_files/build.sh
@@ -13,7 +13,9 @@ cp -avf "/ctx/system_files"/. /
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux
+dnf5 install -y tmux \
+	starship \
+	kitty
 
 # Use a COPR Example:
 #
